650227890bd81e4913b86a771cbefede556efc46
diane <leshlesh1337@gmail.com>
2024-07-10 10:35:44 -0500

    added some styling for better distinction

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,10 +4,10 @@ import {Outlet} from "react-router-dom";
 
 const MainLayout: FC = () => {
     return (
-        <div>
+        <>
             <MenuComponent/>
             <Outlet/>
-        </div>
+        </>
     );
 };
 
